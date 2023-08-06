f4cc913fe42a63266e146ccb7ef89dbd98663a77
tiendunglove <94985083+tiendunglove@users.noreply.github.com>
2023-08-07 01:01:26 +0700

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
+                year: 2023,
                 month: 6,
-                day: 28,
+                day: 17,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
